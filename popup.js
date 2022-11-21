3dc324eccbbc6ac68792ff7a15fc580b1929fb9d
PingHuskar <ping47024@icloud.com>
2022-11-21 23:45:32 +0700

    first commit

---
+++ popup.js
@@ -0,0 +1,11 @@
+const openIntel = (pos) => {
+    const lat = parseFloat(pos.coords.latitude).toFixed(6)
+    const lng = parseFloat(pos.coords.longitude).toFixed(6)
+    open(`https://intel.ingress.com/intel?ll=${lat},${lng}&z=17`,"_blank")
+    open(`https://warm-sawine-6a72ec.netlify.app/`,"_blank")
+}
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(openIntel)
+} else { 
+    alert("Geolocation is not supported by this browser.")
+}
