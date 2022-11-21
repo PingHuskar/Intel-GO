72b2225d654059bb94191b11efd292e7a4f2e15a
PingHuskar <ping47024@icloud.com>
2022-11-22 00:02:05 +0700

    add google maps

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ const openIntel = (pos) => {
     const lat = parseFloat(pos.coords.latitude).toFixed(6)
     const lng = parseFloat(pos.coords.longitude).toFixed(6)
     open(`https://intel.ingress.com/intel?ll=${lat},${lng}&z=17`,"_blank")
+    open(`https://www.google.com/maps?daddr=${lat},${lng}`,"_blank")
     open(`https://warm-sawine-6a72ec.netlify.app/`,"_blank")
 }
 if (navigator.geolocation) {
