e97d36f97617de0137c8bffc93d277d50fc3897d
PingHuskar <ping47024@icloud.com>
2022-11-22 23:52:50 +0700

    add bannergress map

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
 const openIntel = (pos) => {
     const lat = parseFloat(pos.coords.latitude).toFixed(6)
     const lng = parseFloat(pos.coords.longitude).toFixed(6)
-    open(`https://intel.ingress.com/intel?ll=${lat},${lng}&z=17`,"_blank")
+    const zoom = 17
+    open(`https://intel.ingress.com/intel?ll=${lat},${lng}&z=${zoom}`,"_blank")
+    open(`https://bannergress.com/map?lat=${lat}&lng=${lng}&zoom=${zoom}`,"_blank")
     open(`https://www.google.com/maps?daddr=${lat},${lng}`,"_blank")
     open(`https://warm-sawine-6a72ec.netlify.app/`,"_blank")
 }
